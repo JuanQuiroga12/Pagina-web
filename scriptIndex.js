ddf4cb823ea2bfee64460cc74f391a24d7e547e0
JuanQuiroga12 <est.juand.quiroga@unimilitar.edu.co>
2024-09-23 01:18:58 -0500

    Navegacion en botones Index

--- scriptIndex.js
+++ scriptIndex.js
@@ -5,3 +5,21 @@ document.getElementById('uneteBtn').addEventListener('click', function() {
     // Redirigir a la página de "Unirme"
     window.location.href = "Unirme.html";
 });
+
+// Capturamos el botón "Conoce como"
+document.getElementById('conoceComoBtn').addEventListener('click', function() {
+    // Desplazar a la sección "¿Cómo funciona?"
+    document.getElementById('section-info').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+});
+
+// Capturamos el botón "Conoce nuestros planes"
+document.getElementById('conocePlanesBtn').addEventListener('click', function() {
+    // Desplazar a la sección "Planes"
+    document.getElementById('planes').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+});
